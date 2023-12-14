96418ee6febb00bae129666abcf6b05a4f1e8e2d
antoinedga <an.gordonalvarez@gmail.com>
2023-12-13 22:20:15 -0500

    part of the dayName Utility

--- src/routes/medicineRoutine/medicineRoutineDay.js
+++ src/routes/medicineRoutine/medicineRoutineDay.js
@@ -2,9 +2,8 @@ var express = require('express');
 var router = express.Router();
 const MedicineDividerUserSchema = require("../../models/medicineDividerUser")
 const MedicineRoutineDayService = require("../../services/MedicineRoutineDayService")
-
-let regx = "(mon|tues|wed|thurs|fri|sat|sun|Mon|Tues|Wed|Thurs|Fri|Sat|Sun|Monday|monday|Tuesday|tuesday|Wednesday|wednesday|Thursday|thursday|Friday|friday|Saturday|saturday|Sunday|sunday|[1-7])"
-
+const {dayNameRegx} = require("../../utils/dayNameUtil")
+let regx = dayNameRegx
 
 router.get(`/:day${regx}`, function (req, res){
     MedicineRoutineDayService.getRoutineByDay(req).then(result => {
